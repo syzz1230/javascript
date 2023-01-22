2e3312bd36febeb6dc42875aa109e66b7d80aeaf
Srijan <srijan300899@gmail.com>
2023-01-22 13:30:00 +0530

    adding jvascipt

---
+++ invoking.js
@@ -0,0 +1,12 @@
+var x = 1
+a()
+b()
+console.log(x);
+
+function a()
+    var x = 100
+    console.log(x)
+
+function b()
+    var x = 1000
+    console.log(x)
---
+++ new.js
@@ -0,0 +1,22 @@
+//(1)learning printing using the console
+
+number = 2+2;
+console.log(number)
+number_1 = 3+3;
+console.log(number_1)
+
+console.log(number+number_1)
+
+
+//(2)leanring the let and const keywordfor declaring the variable
+
+let radius = 55
+let area
+const pi = 3.145468456
+
+
+area = radius*radius*pi
+
+console.log(area)
+
+//(3)
